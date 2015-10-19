0f2de375d100a06d669aade53b991a2a901db24f
Amay Singhal <amaysinghal@live.com>
2015-10-18 21:33:32 -0700

    Remove config files

--- cloud/main.js
+++ cloud/main.js
@@ -1,24 +1,43 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
+
+
+var ACTIVITY_TYPE = {1: "Lunch"}
+
+
+
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+    response.success("Hello world!");
 });
 
 // Stub call for API response for getting activity suggestion
 Parse.Cloud.define("getActivityRecommendations", function(request, response) {
-  var resultJson = [];
-  resultJson.push({'name': 'Sakoon',
+
+    print(request);
+
+
+    var resultJson = [];
+    resultJson.push({'name': 'Sakoon',
                    'rating': 4.5,
                    'location': 'Mountain View, CA',
-                   'posterImage': 'http://travelgenes.com/wp-content/uploads/2014/12/Indian-Food-Not-To-Miss.jpg'});
-  resultJson.push({'name': 'Siva',
+                   'posterImage': 'http://bombaygrilltx.com/wp-content/uploads/2013/09/Kheema-Samosa.png'});
+    resultJson.push({'name': 'Siva',
                    'rating': 3.0,
                    'location': 'Palo Alto, CA',
-                   'posterImage': 'http://images.indiatvnews.com/lifestylelifestyle/2014/food-fact1.jpgg'});
-  resultJson.push({'name': 'Amber Cafe',
+                   'posterImage': 'https://cookshook.files.wordpress.com/2011/06/indian-food1.png'});
+    resultJson.push({'name': 'Amber Cafe',
                    'rating': 4.0,
                    'location': 'Mountain View, CA',
-                   'posterImage': 'http://wallpaper-hd.org/uploads/originals/2015/09/08/food_wallpaper_1366x768_Wonderful_Indian_Food_Wallpaper.jpg'});
-  response.success(resultJson);
+                   'posterImage': 'http://sahibjohnsoncity.com/wp-content/uploads/2015/07/2.png'});
+    resultJson.push({'name': 'Madras Cafe',
+                   'rating': 4.0,
+                   'location': 'Sunnyvale, CA',
+                   'posterImage': 'http://3.bp.blogspot.com/-9YgUhaxHNXI/UmFKYPIC_sI/AAAAAAAAAEI/1cvVK-q5l7U/s400/Janardhan.png'});
+    resultJson.push({'name': 'Chaat Bhavan',
+                   'rating': 3.5,
+                   'location': 'Sunnyvale, CA',
+                   'posterImage': 'http://41.media.tumblr.com/60d0c12f327cb4b2579d49c236cc520c/tumblr_inline_npgw2nB5WV1t09dzn_400.png'});
+
+    response.success(resultJson);
 });
