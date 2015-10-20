daa928c37e682050e1cf005d37b833a871d454e0
Amay Singhal <amaysinghal@live.com>
2015-10-19 19:10:45 -0700

    Refs #1: Definition for fetching user activities

--- cloud/main.js
+++ cloud/main.js
@@ -2,42 +2,106 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 
-
-var ACTIVITY_TYPE = {1: "Lunch"}
-
+/**
+ * Application constants
+ */
+var ACTIVITY_TYPE_DICT = {1: "Lunch",
+                          2: "Dinner"}
 
 
 Parse.Cloud.define("hello", function(request, response) {
     response.success("Hello world!");
 });
 
-// Stub call for API response for getting activity suggestion
-Parse.Cloud.define("getActivityRecommendations", function(request, response) {
+/**
+ * Convention for naming webhooks:  "{object}__{action}"
+ */
 
-    print(request);
+
+/**
+ * Section for UserGroup object webhooks
+ */
+
+/**
+ * Webhook for getting activity recommendations for a user group
+ *
+ * @param groupId          {Int}    (required) - group id for which recommendation needs to generated
+ * @param activityType     {Int}    (optional) - user specified filter for recommendation for activity type (e.g. Lunch, Dinner, etc)
+ * @param acticityDate     {Object} (optional) - user specified filter for recommendation for activity date
+ * @param acticityLocation {Object} (optional) - user specified filter for recommendation for activity location
+ *
+ */
+Parse.Cloud.define("user_group__get_activity_recommendations", function(request, response) {
 
 
     var resultJson = [];
-    resultJson.push({'name': 'Sakoon',
-                   'rating': 4.5,
-                   'location': 'Mountain View, CA',
-                   'posterImage': 'http://bombaygrilltx.com/wp-content/uploads/2013/09/Kheema-Samosa.png'});
-    resultJson.push({'name': 'Siva',
-                   'rating': 3.0,
-                   'location': 'Palo Alto, CA',
-                   'posterImage': 'https://cookshook.files.wordpress.com/2011/06/indian-food1.png'});
-    resultJson.push({'name': 'Amber Cafe',
-                   'rating': 4.0,
-                   'location': 'Mountain View, CA',
-                   'posterImage': 'http://sahibjohnsoncity.com/wp-content/uploads/2015/07/2.png'});
-    resultJson.push({'name': 'Madras Cafe',
-                   'rating': 4.0,
-                   'location': 'Sunnyvale, CA',
-                   'posterImage': 'http://3.bp.blogspot.com/-9YgUhaxHNXI/UmFKYPIC_sI/AAAAAAAAAEI/1cvVK-q5l7U/s400/Janardhan.png'});
-    resultJson.push({'name': 'Chaat Bhavan',
-                   'rating': 3.5,
-                   'location': 'Sunnyvale, CA',
-                   'posterImage': 'http://41.media.tumblr.com/60d0c12f327cb4b2579d49c236cc520c/tumblr_inline_npgw2nB5WV1t09dzn_400.png'});
+    resultJson.push({'id': 1,
+                     'name': 'Sakoon',
+                     'rating': 4.5,
+                     'location': 'Mountain View, CA',
+                     'posterImage': 'https://asocialease.parseapp.com/images/stub/Sakoon.png'});
+    resultJson.push({'id': 2,
+                     'name': 'Siva',
+                     'rating': 3.0,
+                     'location': 'Palo Alto, CA',
+                     'posterImage': 'https://asocialease.parseapp.com/images/stub/Siva.png'});
+    resultJson.push({'id': 3,
+                     'name': 'Amber Cafe',
+                     'rating': 4.0,
+                     'location': 'Mountain View, CA',
+                     'posterImage': 'https://asocialease.parseapp.com/images/stub/AmberCafe.png'});
+    resultJson.push({'id': 4,
+                     'name': 'Madras Cafe',
+                     'rating': 4.0,
+                     'location': 'Sunnyvale, CA',
+                     'posterImage': 'https://asocialease.parseapp.com/images/stub/MadrasCafe.png'});
+    resultJson.push({'id': 5,
+                     'name': 'Chaat Bhavan',
+                     'rating': 3.5,
+                     'location': 'Sunnyvale, CA',
+                     'posterImage': 'https://asocialease.parseapp.com/images/stub/ChaatBhavan.png'});
+
+    response.success(resultJson);
+});
+
+/**
+ * Section for User object webhooks
+ */
+
+/**
+ * Webhook for getting user current activities
+ *
+ * @param userId          {Int}    (required) - User id for which activities needs to fetched
+ *
+ */
+Parse.Cloud.define("user__get_current_activities", function(request, response) {
+
+
+    var resultJson = [];
+    resultJson.push({'id': 1,
+                     'name': 'Lunch at Sakoon',
+                     'rating': 4.5,
+                     'location': 'Mountain View, CA',
+                     'created_at': "Mon Oct 19 11:08:02 +0000 2012",
+                     'happens_at': "Wed Oct 21 12:30:00 +0000 2012",
+                     'posterImage': 'https://asocialease.parseapp.com/images/stub/Sakoon.png',
+                     'group': {'id': 12, 'name': 'F.R.I.E.N.D.S'}});
+    resultJson.push({'id': 2,
+                     'name': 'Happy Hour at Hause Staudt',
+                     'rating': 3.0,
+                     'location': 'Redwood City, CA',
+                     'created_at': "Mon Oct 19 1:18:02 +0000 2012",
+                     'happens_at': "Thu Oct 22 5:30:00 +0000 2012",
+                     'posterImage': 'https://asocialease.parseapp.com/images/stub/HausStaudt.jpeg',
+                     'group': {'id': 12, 'name': 'F.R.I.E.N.D.S'}});
+    resultJson.push({'id': 3,
+                     'name': 'Movie at AMC Mercado',
+                     'rating': 4.0,
+                     'location': 'Santa Clara, CA',
+                     'created_at': "Mon Oct 19 17:40:02 +0000 2012",
+                     'happens_at': "Fri Oct 23 8:30:00 +0000 2012",
+                     'posterImage': 'https://asocialease.parseapp.com/images/stub/AMC.jpeg',
+                     'group': {'id': 15, 'name': 'Grad Friends'}});
 
     response.success(resultJson);
 });
