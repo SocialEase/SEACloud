98b7d87935facc860e0114c2689962a6605f2f2b
ykay <ykay@users.noreply.github.com>
2015-10-24 15:23:01 -0700

    Add endpoint to get groups for a user

--- cloud/main.js
+++ cloud/main.js
@@ -26,9 +26,44 @@ Parse.Cloud.define("hello", function(request, response) {
  */
 
 /**
- * @param groupId          {Int}    (required) - group id for which recommendation needs to generated
+ * @param userId          {String}    (required) - user id to get groups for
  */
 Parse.Cloud.define("user__get_user_groups", function(request, response) {
+	var userId = request.params["userid"];
+    
+	var User = Parse.Object.extend("User");
+	var queryUser = new Parse.Query(User);
+	queryUser.get(userId, {
+		success: function(user) {
+			var groupsString = user.get("groups");
+			var groupIds = groupsString.split(",");
+						
+			var Group = Parse.Object.extend("Group");
+			var query2 = new Parse.Query(Group);
+			query2.containedIn("objectId", groupIds);
+			query2.find({
+				success: function(groups) {
+					                  
+					alert("Successfully retrieved " + groups.length + " groups.");
+					
+					response.success(groups)
+				},
+				error: function(error) {
+					console.log("Error: " + error.code + " " + error.message);
+					response.failure("Error retrieving user's groups")
+				}
+			});
+		},
+		error: function(object, error) {
+			response.failure("Error retrieving")
+		}
+	});
+});
+
+/**
+ * @param groupId          {Int}    (required) - group id for which recommendation needs to generated
+ */
+Parse.Cloud.define("user__get_group_users", function(request, response) {
     var groupId = request.params["groupid"];
     console.log(groupId);
     
