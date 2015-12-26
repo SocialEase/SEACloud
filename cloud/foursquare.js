50fd10d79546ab0e8539d7fd1354615025d274ab
udaymitra <udaymitra4u@gmail.com>
2015-12-25 17:08:11 -0800

    more foursquare stuff

--- cloud/foursquare.js
+++ cloud/foursquare.js
@@ -7,21 +7,18 @@ function getUrlForVenueSearch() {
     + "?client_id=" + FOURSQUARE_CLIENT_ID 
     + "&client_secret=" + FOURSQUARE_CLIENT_SECRECT
     + "&ll=40.7,-74"
+    + "&query=sushi"
     + "&v=20161209";
-    
     return url;
-    // console.log('url: ' + url);
-    // return "https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=VN05TLI5IPNON304RGJVCWDDMJH0JU3BGDAVKI3HCZNYJUP5&client_secret=23O3BZEDCJHV0UGSBIZU5C2QZABC400CESRMMQZMC4RWR0O5&v=20161209";
 }
 
 Parse.Cloud.define("get_foursquare_results", function(request, response) {
     var fqUrl = getUrlForVenueSearch();
     Parse.Cloud.httpRequest({
-        // url: 'https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=VN05TLI5IPNON304RGJVCWDDMJH0JU3BGDAVKI3HCZNYJUP5&client_secret=23O3BZEDCJHV0UGSBIZU5C2QZABC400CESRMMQZMC4RWR0O5&v=20161212',
         url: fqUrl,
         success: function(httpResponse) {
             console.log(httpResponse);
-            response.success(httpResponse.text);
+            response.success(httpResponse.data);
         },
         error: function(httpResponse) {
             console.log(httpResponse);
